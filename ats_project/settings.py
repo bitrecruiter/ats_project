d6e251b98cd7ca08a13a0de0e845afc938b41cbb
Alfie "Azelphur" Day <support@azelphur.com>
2014-10-23 15:30:43 +0100

    Settings.py needs a STATIC_ROOT

--- ats_project/settings.py
+++ ats_project/settings.py
@@ -101,6 +101,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'public')
 
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/accounts/login/'
